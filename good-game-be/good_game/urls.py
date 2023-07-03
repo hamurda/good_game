29c295bcb14587d68a1001cc33291e458a7c7dd7
damla <Damla.Hamurcu@ul.com>
2023-07-03 17:50:17 +1000

    editing url settings

--- good-game-be/good_game/urls.py
+++ good-game-be/good_game/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('game/', include('game.urls'))
+    path('games/', include('game.urls')),
+    path('users/', include('user.urls')),
 ]
--- good-game-be/user/urls.py
+++ good-game-be/user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-#URLConf
+
 urlpatterns = [
-    path('games/', views.say_hello)
+    path('user/', views.say_hello),
 ]
