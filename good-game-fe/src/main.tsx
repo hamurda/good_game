e310a61823556e5436bba8f62641b4afa8d000ee
hamurda <damlahmrc@gmail.com>
2023-10-31 14:00:34 +1100

    Implements dark mode

--- good-game-fe/src/main.tsx
+++ good-game-fe/src/main.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom';
-import {ChakraProvider} from '@chakra-ui/react'
+import {ChakraProvider, ColorModeScript} from '@chakra-ui/react'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import ReactDOM from 'react-dom/client'
 import router from "./routing/routes";
-import './index.css'
+import theme from "./theme";
+import './index.css';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-          <ChakraProvider>
+          <ChakraProvider theme={theme}>
+              <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
               <RouterProvider router={router}/>
               <ReactQueryDevtools/>
           </ChakraProvider>
