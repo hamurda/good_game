3c2cd47f39b1bdb8fe65a45bd95750f25cf706ce
hamurda <damlahmrc@gmail.com>
2023-11-02 16:30:23 +1100

    Handles games with no image

--- good-game-fe/src/components/GameCard.tsx
+++ good-game-fe/src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import {Game} from "../services/GameController";
 import {Card, CardBody, Heading, Image, Text} from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import noImage from "../assets/no-image-placeholder.webp";
 
 export interface Props {
     game: Game
@@ -10,7 +11,7 @@ const GameCard = ({game} :Props) =>{
 
     return(
         <Card borderRadius={10} overflow='hidden'>
-            <Image src={game.background_image} />
+            {game.background_image ? <Image src={game.background_image} /> : <Image src={noImage} />}
             <CardBody>
                 <Heading fontSize='2xl'>{game.name}</Heading>
                 <PlatformIconList platforms={game.platforms}/>
