c048a8fb6b66d0b114d8f83310792ebe58eb276e
hamurda <damlahmrc@gmail.com>
2023-11-03 17:38:14 +1100

    Filters games by platform and genre in the fe

--- good-game-fe/src/components/GameGrid.tsx
+++ good-game-fe/src/components/GameGrid.tsx
@@ -3,10 +3,23 @@ import useGames from "../services/hooks/game/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
+import {GameQuery} from "../App";
 
+interface Props {
+    gameQuery: GameQuery
+}
 
-const GameGrid = () => {
-    const {data:games, error, isLoading} = useGames();
+const GameGrid = ({gameQuery}:Props) => {
+    const {data, error, isLoading} = useGames();
+    let games = data
+
+    if(gameQuery.genre){
+        games = games?.filter(game => game.genres?.find(({id}) => id === gameQuery.genre?.id));
+    }
+
+    if(gameQuery.platform){
+        games = games?.filter(game => game.platforms?.find(({id}) => id === gameQuery.platform?.id));
+    }
 
     if(error) return <Text textAlign='left' fontSize='3xl'>{error.message}</Text>
     const skeletons = [1,2,3,4,5,6];
